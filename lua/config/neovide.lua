56d9a7a20597921ba48c1aadb4417101a51f00af
Артем Старченко <41ways1ucky@gmail.com>
2026-06-10 21:39:39 +0300

    fix

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -1,5 +1,11 @@
 if vim.g.neovide then
-  vim.o.guifont = 'JetBrainsMono Nerd Font:h13'
+  vim.o.guifont = 'JetBrainsMono Nerd Font:h11'
   vim.g.neovide_fullscreen = true
-  vim.g.neovide_cursor_animation_length = 0
+  vim.g.neovide_position_animation_length = 0
+  vim.g.neovide_cursor_animation_length = 0.00
+  vim.g.neovide_cursor_trail_size = 0
+  vim.g.neovide_cursor_animate_in_insert_mode = false
+  vim.g.neovide_cursor_animate_command_line = false
+  vim.g.neovide_scroll_animation_far_lines = 0
+  vim.g.neovide_scroll_animation_length = 0.00
 end
